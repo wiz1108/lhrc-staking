38cbe128c3c300b22958e4bf7a630b3c78f0171c
wiz1108 <166735057+wiz1108@users.noreply.github.com>
2025-12-04 10:21:10 -0500

    Remove unused styled components
    Co-Authored-By: angel4156 <168684641+angel4156@users.noreply.github.com>

--- src/components/FeatureItem/index.js
+++ src/components/FeatureItem/index.js
@@ -4,27 +4,6 @@ import React from 'react';
 // import itemImg from '../../assets/image/1x/Asset 5.png'
 import './featureItem.scss'
 
-// const StyledBody = styledComponents.section`
-//     border-radius: 40px;
-//     padding: 20px;
-//     display: flex;
-//     flex-direction: column;
-//     background-color: #65361c;
-//     border: 3px solid rgb(153, 112, 93);
-// `
-
-// const StyledItemTitle = styledComponents.text`
-//     color: white;
-//     text-shadow: -1px 0 #65361c, 0 1px #65361c, 1px 0 #65361c, 0 -1px #65361c;
-//     font-size: 40px;
-//     margin-top: 20px;
-// `
-
-// const StyledItemDesc = styledComponents.text`
-//     color: rgb(153, 112, 93);
-//     font-size: 20px;
-// `
-
 const FeatureItem = (props) => {
     return (
         <div>
